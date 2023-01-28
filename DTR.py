21f27c11ced74323af82e2dd85257976f2cfa3ed
yldevierjohn <yldeviermagpusao@gmail.com>
2023-01-28 23:48:43 +0800

    Created Register Class in DTR.py

--- DTR.py
+++ DTR.py
@@ -1,22 +1,27 @@
-# Daily Time Record Class
 
-from datetime import datetime   #use the datetime module to detect hours and minutes
+class Register:
 
-class Payroll:
-    def __init__(self, name, rate): #initialize the parameters
-        self.name = name
-        self.rate = rate
+    def __init__(self, file_name):
+        self.file_name = file_name
+
+    def read_file(self):
+        with open(self.file_name, 'r') as file:
+            return file.read()
+    # Override everything in txt file
+    # Used to initiate registration of employees
+    def write_to_file(self, text):
+        with open(self.file_name, 'w') as file:
+            file.write(text)
+    # append date for Time In
+    def appendDate(self, text):
+        with open(self.file_name, 'a') as file:
+            file.write("\n" + text)
+    # append time for Time In
+    def appendTimeIn(self, text):
+        with open(self.file_name, 'a') as file:
+            file.write(" " + text)
+    # append time for Time Out
+    def appendTimeOut(self, text):
+        with open(self.file_name, 'a') as file:
+            file.write(" " + text)
     
-    def calculate_pay(self, arrival, departure):        #method to calculate the pay
-        hours = (departure - arrival).seconds / 3600    #use .seconds rather than hour to convert the minutes to decimal
-        pay = hours * self.rate                         #if .seconds 09:30 will be 9.5, while .hours will be 9 only
-        return pay                                      #divide by 3600 to convert to an integer hour
-
-name = input("Enter employee name: ")
-employee = Payroll(name, 10)
-arrival = input("Enter arrival time (HH:MM): ")
-departure = input("Enter departure time (HH:MM): ")
-arrival = datetime.strptime(arrival, "%H:%M")           #use the %H:%M to not return an error when inputing 08:00, but accept it
-departure = datetime.strptime(departure, "%H:%M")       #%H:%M is a format used in the strptime function, expecting 24 hour format
-pay = employee.calculate_pay(arrival, departure)
-print("Pay for", employee.name, "is $", pay)
