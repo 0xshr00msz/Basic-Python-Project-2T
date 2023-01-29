923e6c95c41f39778589445788426b41be1f7bc1
yldevierjohn <yldeviermagpusao@gmail.com>
2023-01-29 18:30:52 +0800

    Updated test.py

--- test.py
+++ test.py
@@ -5,8 +5,6 @@ import os
 date = datetime.datetime.now()
 newDate = datetime.datetime(2022, 1, 29)
 
-# fileHandler = Register("" + ".txt")
-
 def reg():
     reg = input("Enter your name to register: ")
     return reg
@@ -18,15 +16,14 @@ def timeOut():
     return reg
 
 menu = int(input('Select your choice \n(1) Register\n(2)Time in\n(3)Time out\nEnter your Choice: '))
+
 if menu == 1:
     fileHandler = Register(reg() + ".txt")
     fileHandler.write_to_file("")
 if menu == 2:
     fileHandler = Register(timeIn() + ".txt")
-
     filePath = 'FileHandling'
     fileName = fileHandler.file_name
-
     
     if os.path.exists(fileName):
         fileHandler.appendDate(date.strftime("%m") + "/" + date.strftime("%d") + "/" + date.strftime("%Y"))
@@ -34,10 +31,12 @@ if menu == 2:
     else: 
         print('Employee does not exist. Please register your name.')
 elif menu == 3:
-    fileHandler = Register(timeOut())
-    fileHandler.appendTimeOut(date.strftime("%I") +  ":" + date.strftime("%M") + date.strftime("%p"))
+    fileHandler = Register(timeOut() + '.txt')
+    filePath = filePath = 'FileHandling'
+    fileName = fileHandler.file_name
 
+    if os.path.exists(fileName):
+        fileHandler.appendTimeOut(date.strftime("%I") +  ":" + date.strftime("%M") + date.strftime("%p"))
+    else:
+        print("Employee does not exist. Please register your name.")
 
-# fileHandler.append_to_file(newDate.strftime("%b") + " " + newDate.strftime("%d") + " " + newDate.strftime("%Y") + ": ")
-# fileHandler.write_to_file(date.strftime("%b") + " " + date.strftime("%d") + " " + date.strftime("%Y") + ": ")
-#     print(fileHandler.read_file())
