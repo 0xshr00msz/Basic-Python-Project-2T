c69133bbaf51591ea553a742c93ad81ace04869f
yldevierjohn <yldeviermagpusao@gmail.com>
2023-02-01 00:20:06 +0800

    Variables renamed

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from DTR import Register   # From Register.py file Register Class
 import datetime
 import os
 
-class TimeClock:
+class ReadFile:
     def __init__(self):
         self.date = datetime.datetime.now()
 
@@ -10,15 +10,15 @@ class TimeClock:
         reg = input("Enter your name to register: ")
         return reg
 
-    def time_in(self):
+    def timeIn(self):
         reg = input('Enter your name to time in: ')
         return reg
 
-    def time_out(self):
+    def timeOut(self):
         reg = input('Enter your name to time out: ')
         return reg
 
-    def run(self):
+    def main(self):
         while True:
             menu = input('Select your choice \n(1) Register\n(2) Time in\n(3) Time out\nEnter your Choice: ')
             if not menu.isdigit():
@@ -27,30 +27,34 @@ class TimeClock:
             menu = int(menu)
 
             if menu == 1:
-                file_handler = Register(self.reg() + ".csv")
-                file_handler.write_to_file("")
+                fileHandler = Register(self.reg() + ".csv")
+                fileHandler.write_to_file("")
             elif menu == 2:
-                file_handler = Register(self.time_in() + ".csv")
-                file_name = file_handler.file_name
+                fileHandler = Register(self.timeIn() + ".csv")
+                fileHandler = fileHandler.file_name
 
-                if os.path.exists(file_name):
-                    file_handler.appendDate(self.date.strftime("%m") + "/" + self.date.strftime("%d") + "/" + self.date.strftime("%Y"))
-                    file_handler.appendTimeIn(self.date.strftime("%I") +  ":" + self.date.strftime("%M") + self.date.strftime("%p"))
+                if os.path.exists(fileHandler):
+                    fileHandler.appendDate(self.date.strftime("%m") + "/" + self.date.strftime("%d") + "/" + self.date.strftime("%Y"))
+                    fileHandler.appendTimeIn(self.date.strftime("%I") +  ":" + self.date.strftime("%M") + self.date.strftime("%p"))
                 else:
                     print('Employee does not exist. Please register your name.')
             elif menu == 3:
-                file_handler = Register(self.time_out() + '.csv')
-                file_name = file_handler.file_name
+                fileHandler = Register(self.timeOut() + '.csv')
+                fileHandler = fileHandler.file_name
 
-                if os.path.exists(file_name):
-                    file_handler.appendTimeOut(self.date.strftime("%I") +  ":" + self.date.strftime("%M") + self.date.strftime("%p"))
+                if os.path.exists(fileHandler):
+                    fileHandler.appendTimeOut(self.date.strftime("%I") +  ":" + self.date.strftime("%M") + self.date.strftime("%p"))
                 else:
                     print("Employee does not exist. Please register your name.")
                 
-            go_back = input('Do you want to go back to the menu? (y/n) ')
-            if go_back.lower() == 'n':
+            goBack = input('Do you want to go back to the menu? (y/n) ')
+            if goBack.lower() == 'n':
+                break
+            elif goBack.lower() == 'y':
+                continue
+            else:
                 break
 
 if __name__ == "__main__":
-    time_clock = TimeClock()
-    time_clock.run()
+    time_clock = ReadFile()
+    time_clock.main()
