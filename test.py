56f627a2281cfcfb251cf1d3cb61d46557bee7cd
Janrevolution <122196668+Janrevolution@users.noreply.github.com>
2023-02-06 14:14:28 +0800

    Added error handling for timeOut and exit option

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ import os
 class ReadFile:
     def __init__(self):
         self.date = datetime.datetime.now()
+        self.time_in_data = {}
 
     def reg(self):
         reg = input("Enter your name to register: ")
@@ -12,41 +13,47 @@ class ReadFile:
 
     def timeIn(self):
         reg = input('Enter your name to time in: ')
+        self.time_in_data[reg] = self.date
         return reg
 
     def timeOut(self):
         reg = input('Enter your name to time out: ')
-        return reg
+        if reg in self.time_in_data:
+            return reg
+        else:
+            print("Enter time in first before timing out")
+            return None
 
     def main(self):
         while True:
-            menu = input('Select your choice \n(1) Register\n(2) Time in\n(3) Time out\nEnter your Choice: ')
+            menu = input('Select your choice \n(1) Register\n(2) Time in\n(3) Time out\n(4) Exit the terminal\nEnter your Choice: ')
             if not menu.isdigit():
                 print('Invalid input. Please enter a number between 1 and 3.')
                 continue
             menu = int(menu)
 
             if menu == 1:
-                fileHandler = Register(self.reg() + ".xlsx")
+                fileHandler = Register(self.reg() + ".csv")
                 fileHandler.write_to_file("")
             elif menu == 2:
-                fileHandler = Register(self.timeIn() + ".xlsx")
-                fileHandler = fileHandler.file_name
-
-                if os.path.exists(fileHandler):
+                name = self.timeIn()
+                fileHandler = Register(name + ".csv")
+                if os.path.exists(fileHandler.file_name):
                     fileHandler.appendDate(self.date.strftime("%m") + "/" + self.date.strftime("%d") + "/" + self.date.strftime("%Y"))
                     fileHandler.appendTimeIn(self.date.strftime("%I") +  ":" + self.date.strftime("%M") + self.date.strftime("%p"))
                 else:
                     print('Employee does not exist. Please register your name.')
             elif menu == 3:
-                fileHandler = Register(self.timeOut() + '.xlsx')
-                fileHandler = fileHandler.file_name
+                name = self.timeOut()
+                if name:
+                    fileHandler = Register(name + '.csv')
+                    if os.path.exists(fileHandler.file_name):
+                        fileHandler.appendTimeOut(self.date.strftime("%I") +  ":" + self.date.strftime("%M") + self.date.strftime("%p"))
+                    else:
+                        print("Employee does not exist. Please register your name.")
+            elif menu == 4:
+                break
 
-                if os.path.exists(fileHandler):
-                    fileHandler.appendTimeOut(self.date.strftime("%I") +  ":" + self.date.strftime("%M") + self.date.strftime("%p"))
-                else:
-                    print("Employee does not exist. Please register your name.")
-                
             goBack = input('Do you want to go back to the menu? (y/n) ')
             if goBack.lower() == 'n':
                 break
