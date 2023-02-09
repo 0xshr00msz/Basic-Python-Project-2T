72e9908c61a240e8c709058fbfc8ef0ccc1774a2
Janrevolution <122196668+Janrevolution@users.noreply.github.com>
2023-02-09 14:29:45 +0800

    added handling error to goBack

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
 from DTR import Register   # From Register.py file Register Class
-from Payroll import Payroll
 import datetime
 import os
 
@@ -29,7 +28,7 @@ class ReadFile:
         while True:
             menu = input('Select your choice \n(1) Register\n(2) Time in\n(3) Time out\n(4) Calculate Pay\n(5) Exit the Terminal\nEnter your Choice: ')
             if not menu.isdigit():
-                print('Invalid input. Please enter a number between 1 and 3.')
+                print('Invalid input. Please enter a number between 1 and 5.')
                 continue
             menu = int(menu)
 
@@ -53,19 +52,21 @@ class ReadFile:
                     else:
                         print("Employee does not exist. Please register your name.")
             elif menu == 4:
-                file_name = input("Enter the name of the Employee: ")
-                register = Payroll(file_name)
+                file_name = input("Enter the name of the file: ")
+                register = Register(file_name)
                 register.read()
             elif menu == 5:
                 break
 
-            goBack = input('Do you want to go back to the menu? (y/n) ')
-            if goBack.lower() == 'n':
-                break
-            elif goBack.lower() == 'y':
-                continue
-            else:
-                break
+            while True:
+                goBack = input('Do you want to go back to the menu? (y/n) ')
+                if goBack.lower() == 'n':
+                    break
+                elif goBack.lower() == 'y':
+                    break
+                else:
+                    print("Only input Y/y or N/n")
+        return
                 
 if __name__ == "__main__":
     time_clock = ReadFile()
